5afa64907bb904b091c22bfc15732c79ca11e5e6
salman354 <mskhn2255@gmail.com>
2022-12-05 14:10:03 +0530

    Created docker file

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask,redirect, url_for, request,render_template
 
-application=Flask(__name__)
+app=Flask(__name__)
 
 
-@application.route('/')
+@app.route('/')
 def home():
     return render_template('home.html')
 
 
 if __name__=="__main__":
-    application.run(debug=True)
+    app.run(debug=True)
